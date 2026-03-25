497e480a3a5a6a8006a22981f796c2cb5fb2cf55
ivan-teotonio <ivanteotonio256@gmail.com>
2026-03-25 20:04:12 -0300

    Criado a controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/controller/NinjaController.java
@@ -0,0 +1,18 @@
+package dev.java10x.CadastroDeNinjas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem";
+    }
+    
+}
