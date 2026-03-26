f49e898ed0097a573ca898cdaf24297275920c1c
ivan-teotonio <ivanteotonio256@gmail.com>
2026-03-25 23:33:03 -0300

    Cria uma entidade no banco de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/model/NinjaModel.java
@@ -1,13 +1,18 @@
 package dev.java10x.CadastroDeNinjas.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_cadastro")
 public class NinjaModel {
 
-    public Long id;
+    @Id 
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    Long id;
     private String nome;
     private String email;
     private int idade;
